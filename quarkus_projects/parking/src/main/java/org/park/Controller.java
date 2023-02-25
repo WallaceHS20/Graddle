6c4039bef6243539a9fec7bbc967a2b28bf3d84f
Wallace Honorato da Silva <101594950+WallaceHS20@users.noreply.github.com>
2023-02-24 21:27:21 -0300

    UPDATE PROJECT

--- quarkus_projects/parking/src/main/java/org/park/Controller.java
+++ quarkus_projects/parking/src/main/java/org/park/Controller.java
@@ -29,6 +29,15 @@ public class Controller {
     	return Response.status(200).entity(json).build();	
     }
 	
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{modelo}")
+    public Response carro2(@PathParam String modelo) {	
+		List<Carro> carrosEncontrados = model.buscarModelo(modelo);	
+		String json = new Gson().toJson(carrosEncontrados);
+    	return Response.status(200).entity(json).build();	
+    }
+	
 	
 	
 	
